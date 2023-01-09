7a9d16c88cff62b1050cd3b109fb5b2fbb98b6ac
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2023-01-09 15:50:52 +0100

    Stesura Controller e View. Creazione della classe utility PizzaData.

---
+++ WebApplicationTest/Controllers/PizzaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationTest.Models;
+using WebApplicationTest.Utilities;
+
+namespace WebApplicationTest.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizzaList = PizzaData.ConstructPizzas();
+
+            return View("Index", pizzaList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            List<Pizza> pizzaList = PizzaData.ConstructPizzas();
+
+            foreach(Pizza element in pizzaList)
+            {
+                if(element.Id == id)
+                {
+                    return View(element);
+                }
+            }
+
+            return NotFound("Something went wrong tralalala");
+        }
+    }
+}
---
+++ WebApplicationTest/Utilities/PizzaData.cs
@@ -0,0 +1,28 @@
+﻿using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Utilities
+{
+    public static class PizzaData
+    {
+        private static List<Pizza> pizzas;
+
+        public static List<Pizza> ConstructPizzas()
+        {
+            if(pizzas != null )
+            {
+                return pizzas;
+            }
+
+            pizzas = new List<Pizza>();
+
+            for(int i=0; i<10; i++)
+            {
+                Pizza newPizza = new(i, "Nome Pizza", "Descrizione della pizza", "https://picsum.photos/200/?random=" + i, "" + i + ",50€");
+                pizzas.Add(newPizza);
+            }
+
+
+            return pizzas;
+        }
+    }
+}
